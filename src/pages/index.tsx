b423403928ff867689beefa4d05734b5bd2dd7ee
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-04-12 16:37:45 +0200

    retrieving db data example (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,34 @@
+import { PrismaClient } from '@prisma/client'
 import type { NextPage } from 'next'
 
-const Home: NextPage = () => {
-  return <button>hello: {"test"}</button>
+export async function getStaticProps() {
+  const prisma = new PrismaClient()
+  const detections = await prisma.detection.findMany()
+
+  return {
+    props : { detections }
+  }
+}
+
+export type DetectionProps = {
+  id: number;
+  objects: number;
+  types: string;
+  lat: number;
+  lon: number;
+  picture: string;
+};
+
+type Props = {
+  detections: DetectionProps[];
+};
+
+const Home: React.FC<Props> = (props) => {
+
+  return <div>
+    {props.detections.map((detection) => (
+      <div key={detection.id}>{detection.id}</div>) )}
+  </div>
 }
 
 export default Home
