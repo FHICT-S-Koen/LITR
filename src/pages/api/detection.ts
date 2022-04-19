0281cc7172d3e3b243938ba7cc32ed7a1b7ddbbb
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-04-19 15:11:30 +0200

    added simple authentication (#49)

--- src/pages/api/detection.ts
+++ src/pages/api/detection.ts
@@ -10,11 +10,20 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<DetectionProps | Error>
 ) {
-  const body = JSON.parse(req.body)
-  if (req.method === 'POST') {
-    const detection = await prisma.detection.create({ data: { ...body} })
-    res.status(201).json(detection)
-  } else {
-    res.status(404).json({ error: '404 error' })
+  try {
+    const body = req.body as DetectionProps
+    if (req.headers['authorization'] === process.env.SECRET_KEY) {
+      if (req.method === 'POST') {
+        const detection = await prisma.detection.create({ data: { ...body } })
+        res.status(201).json(detection)
+      } else {
+        res.status(404).json({ error: '404 error' })
+      }
+    } else {
+      res.status(401).json({ error: '401 error' })
+    }
+  }
+  catch (err) {
+    res.status(500).json({ error: '500 error' })
   }
 }
