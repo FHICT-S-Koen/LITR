15479761bfa9fb0b61f883dd4c130138c5a73740
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-04-26 12:33:51 +0200

    moved test.py code to main.py, added detect.py

---
+++ sensor/detect.py
@@ -0,0 +1,53 @@
+import json
+import cv2
+import datetime 
+import requests
+
+from objects import Detection
+
+def detect(model, API_URL: str, SECRET_KEY: str):
+
+	# Detect real time
+	cap = cv2.VideoCapture(0)
+
+	isRunning = True
+
+	while isRunning:
+		ret, frame = cap.read()    
+
+		# Make detections 
+		results = model(frame)
+
+		# cv2.imshow('YOLOv5s', np.squeeze(results.render()))
+
+		# time location littertype probability image(frames) bounding box
+		objects = []
+		detectedAt = datetime.datetime.now().strftime("%c")
+		lat = 00.00
+		lon = 00.00   
+		picture = results.render()
+
+		for n in results.xyxyn[0]:
+			classIndex = int(n[5])
+			type = results.names[classIndex]
+			probability = n[4]
+
+			xMin = results.xyxy[0][0]
+			yMin = results.xyxy[0][1]
+			xMax = results.xyxy[0][2]
+			yMax = results.xyxy[0][3]
+
+
+			objects.append({type, probability, xMin, yMin, xMax, yMax})
+
+		data = json.dumps(Detection(objects, detectedAt, lat, lon, "picture" ).__dict__)
+
+		res = requests.post(API_URL, data=data, headers={"Authorization": SECRET_KEY})
+		# print(res.text)
+		isRunning = False
+
+		if cv2.waitKey(10) & 0xFF == ord('q'):
+			break
+	cap.release()
+
+	cv2.destroyAllWindows()
--- sensor/main.py
+++ sensor/main.py
@@ -1,43 +1,40 @@
+
+from matplotlib.font_manager import json_dump
 import torch
+from matplotlib import pyplot as plt
 import numpy as np
-import cv2
+
+import os
+from dotenv import load_dotenv
+
+from detect import detect
 
 # setting device on GPU if available, else CPU
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print('Using device:', device)
-print()
+print('Using device:', device, '\n')
 
 #Additional Info when using cuda
-if device.type == 'cuda':
-    print(torch.cuda.get_device_name(0))
-    print('Memory Usage:')
-    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-    print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+# if device.type == 'cuda':
+#     print(torch.cuda.get_device_name(0))
+#     print('Memory Usage:')
+#     print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+#     print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
 
-#Load Model (yolov5s)
+# Load Model (yolov5s)
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-# Set IOU treshold
-# model.iou = 0.3
+
 # Set confidence treshold
-model.conf = 0.3
+model.conf = 0.30
+
 # Only detect bottles
 model.classes = [39]
 
-# Detect real time
-cap = cv2.VideoCapture(0)
-while cap.isOpened():
-    ret, frame = cap.read()
-    
-    # Make detections 
-    results = model(frame)
-    
-    cv2.imshow('YOLOv5s', np.squeeze(results.render()))   
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+load_dotenv() # DO NOT PRINT OUT THE ENVIRONMENT VARIABLES since the output may be saved, because of Jupyter notebook and then may be pushed to github excedentally
 
+API_URL = os.getenv('API_URL')
+if API_URL is None: raise Exception("API_URL can't be empty")
 
+SECRET_KEY = os.getenv('SECRET_KEY')
+if SECRET_KEY is None: raise Exception("SECRET_KEY can't be empty")
 
-
+detect(model, API_URL, SECRET_KEY)
