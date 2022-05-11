2331604e9bd46ab28b2847aa4209a801ad7f9e16
Tsvetan Topalov <tmtopalov@gmail.com>
2022-05-11 16:26:12 +0200

    firefox fix

--- heatmap/src/components/SearchBar.tsx
+++ heatmap/src/components/SearchBar.tsx
@@ -1,13 +1,17 @@
+import { map } from 'leaflet'
 import React from 'react'
 
 const SearchBar = ({}) => {
+  const list = [
+    {name:"Eindhoven", coords:{lat:51.441643, lon:5.469722}},
+    {name:"Amsterdam", coords:{lat:52.370216, lon:4.895168}},
+    {name:"Breda", coords:{lat:51.571915, lon:4.768323}}
+  ]
   return (
     <div className='flex justify-center mb-3 xl:w-96'>
       <input onChange={e => console.log(e.target.value)} list="cities" className='absolute z-[999] bg-white left-1/2 -translate-x-1/2 px-3 py-1.5 text-base font-normal text-gray-700 bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none ' placeholder='Enter City' v-model="textInput"/>
         <datalist id="cities">
-          <option value="Eindhoven">51.441643 5.469722</option>
-          <option value="Amsterdam">52.370216 4.895168</option>
-          <option value="Breda">51.571915 4.768323</option>
+          {list.map((m, i) => <option key={i} value={m.name}>{m.name}</option>)}
         </datalist>
     </div>
   )
