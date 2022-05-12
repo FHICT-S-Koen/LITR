3ae822b2a7bd14e938fe818340ba312fd43fb585
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-05-12 14:01:59 +0200

    Converted get_location string tuple into int (#67 )

--- sensor/src/sensor.py
+++ sensor/src/sensor.py
@@ -1,9 +1,11 @@
 import serial
 
 # make sure the 'COM#' is set according the Windows Device Manager
-def get_location(): #//TODO: fix, (currently return default values, eventually we want to return the actual location)
-    # ser = serial.Serial('COM3', 9600, timeout=1)
-    # val = ser.readline()
-    # ser.close()
-    # return val
-    return 0, 0
+def get_location(): 
+    try: 
+        ser = serial.Serial('COM3', 9600, timeout=1)
+        val = ser.readline().split(' ')
+        ser.close()
+        return int(val[0]), int(val[1])
+    except:
+        return 0, 0
