8da79bf99edc0522fe050cc23293d24096035911
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-05-18 20:32:57 +0200

    changed sensor code to run continuously even when required devices are not connected

--- sensor/src/main.py
+++ sensor/src/main.py
@@ -1,6 +1,8 @@
+import time
 import torch
 import requests
 import cv2
+import serial
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -25,39 +27,46 @@ model.classes = [39]
 # Get environment variables
 API_URL, SECRET_KEY = _utils.get_env(['API_URL', 'SECRET_KEY'])
 
-# Start videocapture
-cap = cv2.VideoCapture(0)
-count = 0
+while True:
+	try:
+		# Try to start videocapture
+		cap = cv2.VideoCapture(0)
+		print("Camera connected")
+		serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+		print("GPS connected")
+		count = 0
 
-# --------------------- START DETECTING ------------------------ #
-while cap.isOpened():
-	if cv2.waitKey(10) & 0xFF == ord('h'):
-		break
-	ret, frame = cap.read()
+		# --------------------- START DETECTING ------------------------ #
+		while cap.isOpened():
+			if cv2.waitKey(10) & 0xFF == ord('h'):
+				break
+			ret, frame = cap.read()
 
-	# Check frame for objects
-	results = model(frame)
+			# Check frame for objects
+			results = model(frame)
 
-	# Show results in window
-	# cv2.imshow('YOLOv5s', np.squeeze(results.render()))
+			# Show results in window
+			# cv2.imshow('YOLOv5s', np.squeeze(results.render()))
 
-	# Reformat results to pandas dataframe
-	df = results.pandas().xyxy[0]
+			# Reformat results to pandas dataframe
+			df = results.pandas().xyxy[0]
 
-	# If empty continue with next frame
-	# else send detection data
-	if (df.empty): continue
+			# If empty continue with next frame
+			# else send detection data
+			if (df.empty): continue
 
-	lat, lon = sensor.get_location()
-	res = requests.post(
-		url=API_URL, 
-		data=Detection(df, lat, lon, frame).json_serialize(), # To get the image with bounding box use: results.render()[0]
-		headers={"Authorization": SECRET_KEY, "Content-Type": "application/json"})
-	
-	count += 1
-	print(f'{count} data send')
-	print(res.text)
+			lat, lon = sensor.get_location()
+			res = requests.post(
+				url=API_URL, 
+				data=Detection(df, lat, lon, frame).json_serialize(), # To get the image with bounding box use: results.render()[0]
+				headers={"Authorization": SECRET_KEY, "Content-Type": "application/json"})
+			
+			count += 1
+			print(f'{count} data send')
+			# print(res.text)
+	except:
+		cap.release()
+		cv2.destroyAllWindows()
 
-
-cap.release()
-cv2.destroyAllWindows()
+		time.sleep(1)
+		continue
