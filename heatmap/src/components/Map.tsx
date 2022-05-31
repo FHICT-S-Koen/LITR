e8ffda562f04635322a4f7179c7d913943f90d23
Tsvetan Topalov <tmtopalov@gmail.com>
2022-05-31 15:09:43 +0200

    switching between maps

--- heatmap/src/components/Map.tsx
+++ heatmap/src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { LayersControl, MapContainer, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css'
 import "leaflet-defaulticon-compatibility";
@@ -12,14 +12,38 @@ import MarkerClusterGroup from './MarkerClusterGroup'
 import "leaflet.markercluster/dist/leaflet.markercluster.js"
 import "leaflet.markercluster/dist/MarkerCluster.css"
 import "leaflet.markercluster/dist/MarkerCluster.Default.css"
+import { Control } from 'leaflet';
+
+const {BaseLayer} = LayersControl;
+const maps = [
+	{name:'Night Mode',
+	 attribution:'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> contributors',
+	 url:"https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+	},
+
+	{name:'Day Mode 2',
+	 attribution:'&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> contributors',
+	 url:"https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
+	}
+]
+
 
 const Map: FC<{detections: DetectionProps[]}> = ({detections}) => {
-	return <MapContainer center={[52.1009, 5.6463]} zoom={9} scrollWheelZoom={true} className="h-screen">
-		<TileLayer
-			attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-			url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-		/>
+	return <MapContainer center={[52.1009, 5.6463]} zoom={8} minZoom={8} scrollWheelZoom={true} className="h-screen">
 		<Search />
+		<LayersControl>
+			<BaseLayer checked name="Day Mode">
+				<TileLayer
+					attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					bounds={[[50.6, 3.25], [54, 7.6]]}
+					minNativeZoom={10}
+				/>
+			</BaseLayer>
+				{maps.map(m => <BaseLayer name={m.name}>
+				<TileLayer attribution={m.attribution} url={m.url} bounds={[[50.5, 3.25], [54, 7.6]]} minNativeZoom={10}/>
+			</BaseLayer>)}
+		</LayersControl>
 		<ResetButton />
 		<MarkerClusterGroup>
 			{detections?.map(d => <Detection key={d.id} {...d} />)}
