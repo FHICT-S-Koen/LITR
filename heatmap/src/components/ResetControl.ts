4862aa695ddebc2b5dfa37b4b5936a0b20e128fb
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-06-01 04:32:44 +0200

    Refactored controls using createControlComponent

---
+++ heatmap/src/components/ResetControl.ts
@@ -0,0 +1,32 @@
+import L, { Control } from 'leaflet'
+import { useMap } from 'react-leaflet';
+import { createControlComponent } from '@react-leaflet/core';
+
+const ResetControl = createControlComponent(controlOptions => {
+  const control = new Control(controlOptions)
+	const html =
+    `<button id='button' class='leaflet-bar w-[34px] h-[34px] bg-white'>
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 m-auto" viewBox="0 0 512 512">
+      <path d="M8 190 184 39c16-13 40-3 40 18v80c161 1 288 33 288 185 0 62-40 123-83 154-14 10-33-2-28-18 45-145-22-184-177-186v88c0 21-24 31-40 18L8 227a25 25 0 0 1 0-37z"/>
+      </svg>
+    </button>`
+
+  control.onAdd = () => {
+    const div = L.DomUtil.create("div");
+    div.innerHTML = html
+
+    div.addEventListener("mousedown", L.DomEvent.stopPropagation)
+    div.addEventListener("click", handleClick)
+
+    return div;
+  };
+
+  const map = useMap()
+  
+	const handleClick = () =>
+		map.setView([51.433333, 5.483333], 14) //TODO: Get Location of user
+  
+	return control
+})
+
+export default ResetControl
---
+++ heatmap/src/components/search/SearchControl.ts
@@ -0,0 +1,37 @@
+import L, { Control, LatLngTuple } from 'leaflet'
+import options from './data/options.json'
+import { useMap } from 'react-leaflet';
+import { createControlComponent } from '@react-leaflet/core';
+  
+const SearchControl = createControlComponent(controlOptions => {
+  const control = new Control(controlOptions)
+  const html = [
+    "<input id='search' autocomplete='off' list='municipalities' placeholder='Enter Location' class='fixed text-base top-0 -translate-x-1/2 left-1/2 mt-2 px-3 py-1.5 text-gray-700 border border-gray-300 rounded focus:border-blue-600 outline-none' />",
+    `<datalist id="municipalities">
+      ${options.map(m => `<option value='${m.name}'>${m.name}</option>`)}
+    </datalist>`
+  ].join("\n");
+
+  control.onAdd = () => {
+    const div = L.DomUtil.create("div", "z-0");
+    div.innerHTML = html
+
+    div.addEventListener("mousedown", L.DomEvent.stopPropagation)
+    div.addEventListener("keyup", handleChange)
+
+    return div;
+  };
+
+  const map = useMap()
+
+  function handleChange(e: KeyboardEvent) {
+    const target = e.target as HTMLInputElement;
+    const object = options.find(o => o.name.toLowerCase() == target.value.toLowerCase())
+    if (object != null)
+      map.setView(object.coords as LatLngTuple, 14)
+  }
+
+  return control
+})
+
+export default SearchControl
