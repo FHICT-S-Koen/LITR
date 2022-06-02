11363f927b9da1d6a6f3bd6d2dacd3e76be03eae
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-02 16:56:02 +0200

    Restyled popup close button

--- heatmap/src/components/detection/Detection.tsx
+++ heatmap/src/components/detection/Detection.tsx
@@ -67,19 +67,21 @@ const Detection: FC<DetectionProps> = (props) => {
 		<Popup>
 			<canvas 
 				ref={canvasRef}
-				className="w-full h-full rounded-t-[5px] outline outline-1" />
+				className="w-full h-full rounded-t-[15px] " />	
+			<button className="absolute top-4 left-4 text-white text-center bg-[#00000066] rounded-full p-[6px]">
+				<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className="block h-3 w-3 stroke-white stroke-[4px] overflow: visible;" aria-hidden="true" role="presentation" focusable="false"><path d="m6 6 20 20"></path><path d="m26 6-20 20"></path></svg>
+			</button>
 			<button 
 				onClick={toggleBoundingBoxes}
 				className="w-full font-bold py-2 mt-0.5 shadow hover:shadow-md">
 				show bounding box
 			</button>
-			<div className="p-2 rounded-b-[5px] text-base">
-				Detected at: {detectedAt}
+			<div className="p-2 rounded-b-[15px] text-base">
+				{detectedAt}
 				<div className="leading-6 text-gray-700 my-2">
 					Types of litter: {objects.map(e => e.type)} <br></br>
 					Number of detected objects: {objects.length}
 				</div>
-				Latitude: {lat} Longitude: {lon}
 			</div>
 		</Popup>
 	</Marker>
