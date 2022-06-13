027ef8c901417e9816d056ea67e73d180c67873b
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2022-06-13 14:29:51 +0200

    Resolved conflict

--- heatmap/src/components/SliderControl.tsx
+++ heatmap/src/components/SliderControl.tsx
@@ -1,29 +1,38 @@
 import L, { Control } from 'leaflet'
 import { useMap } from 'react-leaflet'
 import { createControlComponent } from '@react-leaflet/core'
+import { useState } from 'react'
 
 const SliderControl = createControlComponent(controlOptions => {
-    const control = new Control(controlOptions)
-    const html = 
-    `<input type='range' min='1' max='10' class='leaflet-bar slider'/>`
-    
-    control.onAdd = () => {
-      const div = L.DomUtil.create("div", "bg-white p-1 rounded");
-      div.innerHTML = html
+      const control = new Control(controlOptions)
+      const html = 
+      `<input type='range' min='1' max='5' step='1' class='leaflet-bar slider'/>`
+
+      control.onAdd = () => {
+            const div = L.DomUtil.create("div", "bg-white p-1 rounded");
+            div.innerHTML = html
+
+            L.DomEvent.disableClickPropagation(div)
+            div.addEventListener("input", handleChange) 
+            return div;
+      };
+
+      const map = useMap()
+
+      function handleChange(e: Event) {
+            const target = e.target as HTMLInputElement;
+            console.log(target.value)
+            //Today
+            
+            //Three days
+
+            //One week
+
+            //All
+      }
+
+      return control
+
+      })
   
-      L.DomEvent.disableClickPropagation(div)
-      div.addEventListener("keyup", handleChange)
-  
-      return div;
-    };
-  
-    const map = useMap()
-  
-    function handleChange(e: KeyboardEvent) {
-        const target = e.target as HTMLInputElement;
-    }
-  
-    return control
-  })
-  
-  export default SliderControl
+export default SliderControl
